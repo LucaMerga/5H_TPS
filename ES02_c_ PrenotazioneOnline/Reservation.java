58548eb7763fe8472570ea7782c7a8f85bbc7f05
LucaMerga <113595065+LucaMerga@users.noreply.github.com>
2022-11-21 21:10:21 +0100

    Reservation update

--- ES02_c_ PrenotazioneOnline/Reservation.java
+++ ES02_c_ PrenotazioneOnline/Reservation.java
@@ -1,31 +1,31 @@
 package network;
 
 public class Reservation {
-   
-    private String[] reservations;
 
-    //Costruttore
-    Reservation(int Size) {
-    	reservations=new String[Size];
-    	for (int i=0 ; i<Size ; i++) {
-    		reservations[i]="Posto libero";  //inizialmente tutti i posti sono liberi
-    	}
-    }
-         
-    public synchronized String getReservations() {
-    	String r="";
-    	for (int i=0 ; i<reservations.length ; i++ ) {
-    		r= r + "\n" +  i + " " + reservations[i];
-    	}
-        return r;
-    }
-    
-    public synchronized String setReservation(int posto, String nome) {
-    	String result = "Prenotazione per " + nome + " rifiutata!"; //imposto di default il comando prenotazione rifiutata
-    	if ( reservations[posto].equals("Posto libero")) {
-    		 reservations[posto]=nome;
-    		 result="Prenotazione per " + nome + " accettata!";   //se si sceglie un posto libero la prenotazione viene confermata
-    	} 
-        return result;
-    }
+private String[] reservations;
+
+   //Costruttore
+   Reservation(int Size) {
+      reservations=new String[Size];
+      for (int i=0 ; i<Size ; i++) {
+         reservations[i]="Posto libero";  //inizialmente tutti i posti sono liberi
+      }
+   }
+
+   public synchronized String getReservations() {
+      String r="";
+      for (int i=0 ; i<reservations.length ; i++ ) {
+         r= r + "\n" +  i + " " + reservations[i];
+      }
+      return r;
+   }
+
+   public synchronized String setReservation(int posto, String nome) {
+      String result = "Prenotazione per " + nome + " rifiutata!"; //imposto di default il comando prenotazione rifiutata
+      if ( reservations[posto].equals("Posto libero")) {
+         reservations[posto]=nome;
+         result="Prenotazione per " + nome + " accettata!";   //se si sceglie un posto libero la prenotazione viene confermata
+      } 
+      return result;
+   }
 }
