a517d3b4354365d174e821d7679b9b13e99365aa
LucaMerga <113595065+LucaMerga@users.noreply.github.com>
2022-11-21 20:54:26 +0100

    Reservation update

--- ES02_c_ PrenotazioneOnline/Reservation.java
+++ ES02_c_ PrenotazioneOnline/Reservation.java
@@ -5,10 +5,10 @@ public class Reservation {
     private String[] reservations;
 
     //Costruttore
-    Reservation(int Size ) {
+    Reservation(int Size) {
     	reservations=new String[Size];
-    	for (int i=0 ; i<Size ; i++ ) {
-    		reservations[i]="Posto libero";
+    	for (int i=0 ; i<Size ; i++) {
+    		reservations[i]="Posto libero";  //inizialmente tutti i posti sono liberi
     	}
     }
          
@@ -20,11 +20,11 @@ public class Reservation {
         return r;
     }
     
-    public synchronized String setReservation( int posto, String nome ) {
-    	String result = "Prenotazione per " + nome + " rifiutata!";
+    public synchronized String setReservation(int posto, String nome) {
+    	String result = "Prenotazione per " + nome + " rifiutata!"; //imposto di default il comando prenotazione rifiutata
     	if ( reservations[posto].equals("Posto libero")) {
     		 reservations[posto]=nome;
-    		 result="Prenotazione per " + nome + " accettata!";
+    		 result="Prenotazione per " + nome + " accettata!";   //se si sceglie un posto libero la prenotazione viene confermata
     	} 
         return result;
     }
