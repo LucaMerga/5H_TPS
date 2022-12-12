7eccfe8e790d2c6d2d342924bc3ae4eaa3bd879f
LucaMerga <lucamerga@gmail.com>
2022-12-12 18:15:11 +0000

    update

--- ES03_b_ServerWeb/network/TcpServer.java
+++ ES03_b_ServerWeb/network/TcpServer.java
@@ -16,6 +16,7 @@ public class TcpServer {
 		
 		final int SERVER_PORT=8765;
 		String clientMsg = "";
+		String serverMsg = "";
 		
 		try {			 
 			// Creazione del socket sul server e ascolto sulla porta
@@ -25,7 +26,7 @@ public class TcpServer {
 			boolean endConn=false;
 			while(!endConn) {
 				// Attesa della connessione con il client
-				System.out.println("Attesa ricezione dati dal client ....................... \n");
+				System.out.println("\nAttesa ricezione dati dal client ....................... \n");
 				Socket clientSocket = serverSocket.accept();
 				
 				// Create output stream to write data and input stream to read data from socket
@@ -34,18 +35,48 @@ public class TcpServer {
 	
 				// ---------------------------------------------------------
 				//Lettura dati dal client un righa alla volta   
-				while ((clientMsg=inStream.readLine()).length() != 0) {
-					System.out.println(clientMsg);	
-				}  
+				//while ((clientMsg=inStream.readLine()).length() != 0) {
+				clientMsg=inStream.readLine();
+				System.out.println(clientMsg);	
+				//}  
 				// Elaborare qui i dati ricevuti dal client 
 				// ---------------------------------------------------------
+
+				//tolgo sli spazi vouti a inizio e fine e splitto la stringa in ingresso
+				clientMsg.trim();	
+				String clientMsgSplit[]=clientMsg.split("\\s+");
 
 				//Invio dei dati su stream di rete al client
 				clientMsg = "HTTP/1.1 200 OK\r\n";
 				//clientMsg += "Connection: close\r\n";
-				//clientMsg += "Content-Type: text/plain\r\n";
+				clientMsg += "Content-Type: text/html\r\n";
 				clientMsg += "\r\n";
-				clientMsg += "Saluti dal web server Java di Luca Merga";
+				switch(clientMsgSplit[1]) {
+					case "/": 
+						clientMsg += "Saluti dal web server Java di Luca Merga";
+                        break;
+
+                    case "/accendi":
+						serverMsg += "Accensione Luci...\n<b>Luci accese</b>";
+                        break;
+
+                    case "/spegni":
+						serverMsg += "Spegnimento Luci...\n<b>Luci spente</b>";
+                        break;	
+						
+					case "/favicon.ico":
+						serverMsg += "";
+                        break;
+
+					case "/esci":
+						serverMsg += "Fine esecuzione server";
+						endConn = true;
+                        break;
+
+                    default: 
+						serverMsg += "Errore";           
+                }
+				System.out.println(serverMsg + "\n");	
 				outStream.write(clientMsg.getBytes());
 				outStream.flush();
 
